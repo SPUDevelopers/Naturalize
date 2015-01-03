032dc8be434bb82833f19dc75308660a133c3978
Isaac-W <wage.ink@gmail.com>
2015-01-02 20:36:08 -0800

    Change StringHelpers to use standard functions

--- Classes/StringHelpers.h
+++ Classes/StringHelpers.h
@@ -7,35 +7,16 @@
 #include <string>
 
 // Returns a copy of this string converted to lowercase.
-inline std::string StringToLower(std::string str)
-{
-	for (int i = 0; i < str.length(); i++)
-	{
-		str[i] = tolower(str[i]);
-	}
-
-	return str;
-}
+std::string StringToLower(std::string str);
 
 // Returns a copy of this string converted to uppercase.
-inline std::string StringToUpper(std::string str)
-{
-	for (int i = 0; i < str.length(); i++)
-	{
-		str[i] = toupper(str[i]);
-	}
-
-	return str;
-}
+std::string StringToUpper(std::string str);
 
 // Performs a lowercase comparison of strings.
 // The return value indicates the relation of str1 to str2 as follows:
 // < 0 | str1 less than str2
 // = 0 | str1 identical to str2
 // > 0 | str1 greater than str2
-inline int StringCaseCompare(std::string str1, std::string str2)
-{
-	return StringToLower(str1).compare(StringToLower(str2));
-}
+int StringCaseCompare(std::string str1, std::string str2);
 
 #endif
