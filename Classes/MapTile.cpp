305375e73cf4557ae81a5dd18d52b1f32e59e012
Isaac-W <wage.ink@gmail.com>
2015-01-02 20:44:35 -0800

    Initialize member arrays to zeroes

--- Classes/MapTile.cpp
+++ Classes/MapTile.cpp
@@ -8,6 +8,18 @@
 
 MapTile::MapTile()
 {
+	// Init buffs
+	for (int i = 0; i < Naturalize::FACTION_COUNT; i++)
+	{
+		this->atkBuf[i] = 0;
+		this->defBuf[i] = 0;
+	}
+
+	// Init move costs
+	for (int i = 0; i < Naturalize::MOVETYPE_COUNT; i++)
+	{
+		this->moveCost[i] = 0;
+	}
 }
 
 MapTile::~MapTile()
